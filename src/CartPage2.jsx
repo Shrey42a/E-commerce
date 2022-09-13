5e220028c725fe7a60333abb8d5a0d8534286167
Shrey Kumar <akashjaat55242@gmail.com>
2022-09-13 13:11:55 +0530

    emptycart

--- src/CartPage2.jsx
+++ src/CartPage2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CartPage from "./CartPage";
+//import CartPage from "./CartPage";
 import EmptyCart from "./EmptyCart";
 
 function CartPage2() {
@@ -7,7 +7,6 @@ function CartPage2() {
     <>
       <div className="h-max flex flex-col top-60 items-center min-h-screen bg-neutral-100 p-4 pt-12">
         <div className="bg-white space-y-4 w-9/12 min-w-max m-4 p-2 lg:p-12">
-          <CartPage />
           <EmptyCart />
         </div>
       </div>
