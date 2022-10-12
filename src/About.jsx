a314303fb8769014be800c76abb6893b81ac1421
Shrey Kumar <akashjaat55242@gmail.com>
2022-10-12 17:11:09 +0530

    addedlogo

--- src/About.jsx
+++ src/About.jsx
@@ -9,9 +9,9 @@ function About() {
         <div className="flex flex-col w-full p-2 my-20 lg:w-4/5 h-1/2 lg:h-4/5 lg:my-0 lg:flex-row bg10">
             <div className="flex flex-col w-full h-full lg:w-1/2">
               <div className="w-full lg:h-4/5">
-              <img className="h-auto max-w-full animate__delay__4s animate__slow animate__zoomInUp animate__animated ref" src="https://media.discordapp.net/attachments/933654325127938101/1025607932768960613/Logo4.png?width=789&height=492" alt="..."></img></div>
+              <img className="h-auto max-w-full animate__delay__4s animate__slow animate__zoomInUp animate__animated ref" src="https://media.discordapp.net/attachments/933654325127938101/1029717959393943632/IMG_20221012_165911-removebg-preview.png" alt="..."></img></div>
               <div className="flex items-center justify-center w-full h-24 lg:h-1/5">
-              <img className="h-auto max-w-full animate__delay__4s animate__slow animate__zoomInUp animate__animated ref" src="https://cdn.discordapp.com/attachments/933654325127938101/1028275924665380914/Sat_8_10_2022_17_30_27.png"></img></div>
+              <img className="h-auto max-w-full animate__delay__4s animate__slow animate__zoomInUp animate__animated ref" src="https://media.discordapp.net/attachments/933654325127938101/1029697642109882388/Wed_12_10_2022_15_40_27.png?width=454&height=454"></img></div>
            </div>
             <div className="flex flex-col items-center w-full h-full p-4 py-4 lg:w-1/2 lg:py-16">
              <h1 className="text-2xl font-semibold text-gray-400 underline lg:text-5xl">HI THIS IS @42 SHOPPING</h1>  
