ef7da03d5aad45de319010ee892720ec8e46b645
Shrey Kumar <akashjaat55242@gmail.com>
2022-10-14 17:12:46 +0530

    addedhyt

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -58,11 +58,11 @@ function Navbar({ productCount, setUser, user }) {
                 </li>
                 <li>{user && <button className="h-8 p-1 text-black rounded-lg button3 bg10" onClick={handleLogout}>Logout</button>}</li>
               </ul>
-              <Link to="/cart"><div className="flex">
+              <Link to="/cart"><div className="hidden bg15 lg:block">
                 
-                  <svg className="bg15 bi bi-cart3" xmlns="http://www.w3.org/2000/svg" width="48" height="56" fill="currentColor" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="#1c1c1c"></path> </svg>
+                  <svg className="bi bi-cart3" xmlns="http://www.w3.org/2000/svg" width="48" height="56" fill="currentColor" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" fill="#1c1c1c"></path> </svg>
                 
-                <span className="w-2 -mt-3 font-semibold text-gray-900">
+                <span className="w-2 -mt-20 font-semibold text-gray-900">
                   {productCount}
                 </span>
               </div></Link>
